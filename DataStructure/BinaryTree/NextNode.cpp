1fce3bea018e94be7d01adbeb4b88449b82d8951
XHfight <XHfight@163.com>
2017-06-07 23:27:15 +0800

    二叉树的下一个结点

--- DataStructure/BinaryTree/NextNode.cpp
+++ DataStructure/BinaryTree/NextNode.cpp
@@ -5,7 +5,45 @@
 ＃include <iostream>
 using namespace std;
 
-int main()
-{
-	return 0;
-}
+/*
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+        
+    }
+};
+*/
+
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+	if(pNode == NULL)
+		return NULL;
+   	if(pNode->right != NULL)//右子树不为空，下一节点为右子树的最左节点。
+	{
+		TreeLinkNode* cur = pNode->right;
+		while(cur && cur->left)
+		{
+			cur = cur->left;
+		}
+		return cur;
+	}
+	else //右子树为空
+	{
+		while(pNode->next)
+		{
+			TreeLinkNode* parent = pNode->next;
+			if(parent->left == pNode)
+			{
+				return parent;
+			}
+			pNode = parent;
+		}
+		return NULL;
+	}
+    }
+};
