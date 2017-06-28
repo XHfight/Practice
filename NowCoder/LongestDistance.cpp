efb78f927802d6381321d63094008f35037a37d8
XHfight <X_H_fight@163.com>
2017-06-28 14:39:23 +0800

    最大差值

--- NowCoder/LongestDistance.cpp
+++ NowCoder/LongestDistance.cpp
@@ -14,18 +14,43 @@ using namespace std;
 
 class LongestDistance {
 public:
+	//时间复杂度O(N^2)
     int getDis(vector<int> A, int n) {
         // write code here
         int maxSub = -1;
-        for(int i = 0; i< n; ++i)
-        {
-            for(int j = i; j < n; ++j)
-            {
-                int sub = A[j]-A[i];
-                if(sub > maxSub)
-                    maxSub = sub;
-            }
-        }
+        if(n > 0)
+		{
+			for(int i = 0; i< n; ++i)
+			{
+				for(int j = i; j < n; ++j)
+				{
+					int sub = A[j]-A[i];
+					if(sub > maxSub)
+						maxSub = sub;
+		        }
+			 }
+		}
         return maxSub;
     }
+
+	//时间复杂度哦：O(N)
+	int getDis_2(vector<int> A, int n)
+	{
+		int maxSub = -1;
+		if(n > 0)
+		{
+            int min = A[0];
+            int sub = -1;
+			for(int i = 1; i < n; ++i)
+			{
+				if(A[i] < min)
+					min = A[i];
+
+				sub = A[i]-min;
+				if(sub > maxSub)
+					maxSub = sub;
+			}
+		}
+		return maxSub;
+	}
 };
